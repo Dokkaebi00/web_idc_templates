99a2207169f9ad40f48596962681b2a3bb3007a1
Dokkaebi00 <quang009182@gmail.com>
2021-06-24 21:32:07 +0700

    a

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request, File, Form, UploadFile
-from fastapi import templating
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,23 +6,61 @@ from fastapi.templating import Jinja2Templates
 from PIL import Image
 from io import BytesIO
 
+import json
 import shutil
-import json 
 import base64
 import requests
-import cv2
 import numpy as np
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
-url = "https://"
+url = "http://0.0.0.0:5000/idc_vn/recognition"
+
 
 def load_image_into_numpy_array(data):
     return np.array(Image.open(BytesIO(data)))
+
+
+def sort_result(result):
+    sorted_result = {}
+    id = result["id"] + " -- confidence: " + str(result["id_conf"])
+    sorted_result["ID"] = id
+
+    name = result["name"] + " -- confidence: " + str(result["name_conf"])
+    sorted_result["Name"] = name
+
+    birthday = result["birthday"] + " -- confidence: " + str(result["birthday_conf"])
+    sorted_result["Birthday"] = birthday
+
+    address = result["address"] + " -- confidence: " + str(result["address_conf"])
+    sorted_result["Address"] = address
+
+    hometown = result["hometown"] + " -- confidence: " + str(result["hometown_conf"])
+    sorted_result["Hometown"] = hometown
+
+    document = result["document"]
+
+    if document == "CCCD":
+
+        national = (
+            result["national"] + " -- confidence: " + str(result["national_conf"])
+        )
+        sorted_result["National"] = national
+
+        ethnicity = (
+            result["ethnicity"] + " -- confidence: " + str(result["ethnicity_conf"])
+        )
+        sorted_result["Ethnicity"] = ethnicity
+
+        sorted_result["Document"] = result["document"]
+    elif document == "CMND":
+        sorted_result["Document"] = result["document"]
+    return sorted_result
+
 
 # get index page
 @app.get("/", response_class=HTMLResponse)
@@ -32,76 +69,83 @@ async def index(request: Request):
 
 
 # get result page
-@app.post("/result")
+@app.post("/result", response_class=HTMLResponse)
 async def upload_image(request: Request, image: UploadFile = File(...)):
-    # with open("./static/images/destination.jpg", "wb") as buffer:
-    #     shutil.copyfileobj(image.file, buffer)
+    with open("./static/images/destination.jpg", "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
 
-    image = load_image_into_numpy_array(await image.read())
-    retval, buffer_img= cv2.imencode('.jpg', image)
-    data = base64.b64encode(buffer_img)
-    
-    send_data = {'base64': data}
-    # response = requests.post(url, info = send_data)
+    with open("./static/images/destination.jpg", "rb") as img_file:
+        data = base64.b64encode(img_file.read()).decode("utf-8")
 
-    #test
-    f = open('result.json')
-    result = json.load(f)
+    send_data = {"image": data}
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
-    ###
-    # wait result
-    ###
-
-    # result will be store in a variable named result
-    # f = open('result.json')
-    # result = json.load(f)
-
-    return templates.TemplateResponse("result.html", {"request": request, "result": result})
+    return templates.TemplateResponse(
+        "result.html", {"request": request, "result": result}
+    )
 
 
 # get test_image_1 page
 @app.get("/test_image_1", response_class=HTMLResponse)
 async def test_image_1(request: Request):
-    shutil.copy('./static/images/test_image/ocr-1.jpg', "./static/images/destination.jpg")
+    shutil.copy(
+        "./static/images/test_image/ocr-1.jpg", "./static/images/destination.jpg"
+    )
 
-    #Edit Recognition Code Here
+    with open("./static/images/test_image/ocr-1.jpg", "rb") as img_file:
+        data = base64.b64encode(img_file.read()).decode("utf-8")
 
-    #Edit Recognition Code Here
+    send_data = {"image": data}
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
-    # result will be store in a variable named result
-    f = open('result.json')
-    result = json.load(f)
-
-    return templates.TemplateResponse("result.html", {"request": request, "result": result })
+    return templates.TemplateResponse(
+        "result.html", {"request": request, "result": result}
+    )
 
 
 # get test_image_2 page
 @app.get("/test_image_2", response_class=HTMLResponse)
 async def test_image_2(request: Request):
-    shutil.copy('./static/images/test_image/ocr-2.jpg', "./static/images/destination.jpg")
+    shutil.copy(
+        "./static/images/test_image/ocr-2.jpg", "./static/images/destination.jpg"
+    )
 
-    #Edit Recognition Code Here
+    with open("./static/images/test_image/ocr-2.jpg", "rb") as img_file:
+        data = base64.b64encode(img_file.read()).decode("utf-8")
 
-    #Edit Recognition Code Here
+    send_data = {"image": data}
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
-    # result will be store in a variable named result
-    f = open('result.json')
-    result = json.load(f)
-
-    return templates.TemplateResponse("result.html", {"request": request, "result": result })
+    return templates.TemplateResponse(
+        "result.html", {"request": request, "result": result}
+    )
 
 
 # get test_image_3 page
 @app.get("/test_image_3", response_class=HTMLResponse)
 async def test_image_3(request: Request):
-    shutil.copy('./static/images/test_image/ocr-3.jpg', "./static/images/destination.jpg")
+    shutil.copy(
+        "./static/images/test_image/ocr-3.jpg", "./static/images/destination.jpg"
+    )
 
-    #Edit Recognition Code Here
+    with open("./static/images/test_image/ocr-3.jpg", "rb") as img_file:
+        data = base64.b64encode(img_file.read()).decode("utf-8")
 
-    #Edit Recognition Code Here
+    send_data = {"image": data}
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
-    # result will be store in a variable named result
-    f = open('result.json')
-    result = json.load(f)
-
-    return templates.TemplateResponse("result.html", {"request": request, "result": result })
+    return templates.TemplateResponse(
+        "result.html", {"request": request, "result": result}
+    )
